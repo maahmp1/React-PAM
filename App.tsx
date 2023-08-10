b5ef4ff1c63fef7fa9265d73b188fff02d3b1f74
maahmp1 <marcelamartinspena@gmail.com>
2023-08-10 08:52:57 -0300

    add: logo, style

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  StatusBar, SafeAreaView } from 'react-native';
+import Carrinho from './src/pages/Carrinho/index';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView >
+      <StatusBar/>
+      <Carrinho/>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Carrinho/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Image, StyleSheet } from 'react-native';
+import topo from '../../../assets/topo.png'
+
+export default function Carrinho(){
+    return <Image source={topo} style={estilos.topo}/>
+}
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%"
+    }
+})
