105b59b02add89e4ebd1dcabbd2a0517fe6be991
maahmp1 <marcelamartinspena@gmail.com>
2023-08-17 08:54:40 -0300

    add: css

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,13 +1,41 @@
 import React from "react";
-import { Image, StyleSheet } from 'react-native';
-import topo from '../../../assets/topo.png'
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import topo from "../../../assets/topo.png";
 
-export default function Carrinho(){
-    return <Image source={topo} style={estilos.topo}/>
+const width = Dimensions.get("screen").width;
+
+export default function Carrinho() {
+  return (
+    <>
+      <Image source={topo} style={estilos.topo} />
+      <Text style={estilos.titulo}>Detalhes do Carrinho</Text>
+      <View>
+        <Text>Carrinho de Compras</Text>
+        <Text>Fazenda ETEC Cidade do Livro</Text>
+        <Text>Carrinho de Compras</Text>
+        <Text>
+          Uma cesta com produtos selecionados cuidadosamente da fazenda direto
+          para a sua cozinha
+        </Text>
+        <Text>R$ 40,00</Text>
+      </View>
+    </>
+  );
 }
 
 const estilos = StyleSheet.create({
-    topo: {
-        width: "100%"
-    }
-})
+  topo: {
+    width: "100%",
+    height: (578 / 768) * width,
+  },
+  titulo: {
+    width: "100%",
+    position: "absolute",
+    color: "white",
+    textAlign: "center",
+    fontSize: 24,
+    lineHeight: 26,
+    fontWeight: "bold",
+    padding: 16,
+  },
+});
