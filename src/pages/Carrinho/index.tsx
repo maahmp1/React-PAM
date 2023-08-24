a3e70fbfb73f757c7adb7d5a95b25af8dee1c51e
maahmp1 <marcelamartinspena@gmail.com>
2023-08-24 08:22:10 -0300

    add:styles

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -12,17 +12,19 @@ export default function Carrinho() {
       <Text style={estilos.titulo}>Detalhes do Carrinho</Text>
       <View style={estilos.carrinho}>
         <Text style={estilos.nome}>Carrinho de Compras</Text>
-        <View style={estilos.fazenda}>
-          <Image source={logo}/>
-          <Text>Fazenda ETEC Cidade do Livro</Text>
 
+        <View style={estilos.fazenda}>
+          <Image style={estilos.imagemFazenda }source={logo}/>
+          <Text style={estilos.nomeFazenda} >Fazenda ETEC Cidade do Livro</Text>
         </View>
-        <Text>Carrinho de Compras</Text>
-        <Text>
+
+        <Text style={estilos.descricao}>
           Uma cesta com produtos selecionados cuidadosamente da fazenda direto
           para a sua cozinha
         </Text>
-        <Text>R$ 40,00</Text>
+
+        <Text style={estilos.preco}>R$ 40,00</Text>
+
       </View>
     </>
   );
@@ -57,5 +59,25 @@ const estilos = StyleSheet.create({
   fazenda: {
     flexDirection:"row",
     paddingVertical: 12
+  },
+  imagemFazenda: {
+    width:32,
+    height:32
+  },
+  nomeFazenda:{
+    fontSize: 16,
+    fontFamily: "MontserratBold",
+    lineHeight: 26,
+    marginLeft: 12
+  },
+  descricao: {
+    color: "#A3A3A3",
+    fontFamily: "MontserratItalic"
+  },
+  preco: {
+    fontSize: 26,
+    fontFamily: "MontserratRegular",
+    color: "#2A9F85",
+    marginTop: 8
   }
 });
